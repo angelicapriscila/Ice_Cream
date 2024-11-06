3389addde135b0c19fd182e93cf17b452f506f61
angelicapriscila <apss11@aluno.ifal.edu.br>
2024-11-06 19:27:30 -0300

    Create database.dart

---
+++ database.dart
@@ -0,0 +1,77 @@
+import 'package:helloworld/domain/card_IceCream.dart';
+class Database {
+static List ice = [
+PacoteIce(
+urlimage:
+'https://www.responsivesites.com.br/wp-content/uploads/2020/08
+/cria%C3%A7%C3%A3o-de-logo-para-loja-de-acai.jpg',
+titulo: 'Oficina do Açai',
+valor: 10,
+),
+PacoteIce(
+urlimage:
+'https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize/20
+18/01/31/12/Logo-232544_70207_123405320_1980307652.jpg',
+titulo: 'Cantinho do Açai',
+valor: 10,
+),
+PacoteIce(
+urlimage:
+'https://alexandre-oliveira.com/imgs/2014/10/criacao-logomarca
+-clube-do-acai-bh.jpg',
+titulo: 'Clube do Açai',
+valor: 15,
+),
+PacoteIce(
+urlimage:
+'https://play-lh.googleusercontent.com/CaGc3EcRZMsajoobvA-M735
+cbKgTwMpTJDeDbLczarSNmFbDTctjNR5hD8vlwApV7w',
+titulo: 'Açai expresso',
+valor: 10,
+),
+PacoteIce(
+urlimage:
+'https://th.bing.com/th/id/OIP.cqOCqJmMqIRh2ge8Orj0AQHaHa?rs=1
+&pid=ImgDetMain',
+titulo: 'Açai Brasil',
+valor: 15,
+),
+];
+static List pacotes = [
+PacoteIce(
+urlimage:
+'https://www.responsivesites.com.br/wp-content/uploads/2020/08
+/cria%C3%A7%C3%A3o-de-logo-para-loja-de-acai.jpg',
+titulo: 'Oficina do Açai',
+valor: 10,
+),
+PacoteIce(
+urlimage:
+'https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize/20
+18/01/31/12/Logo-232544_70207_123405320_1980307652.jpg',
+titulo: 'Cantinho do Açai',
+valor: 10,
+),
+PacoteIce(
+urlimage:
+'https://alexandre-oliveira.com/imgs/2014/10/criacao-logomarca
+-clube-do-acai-bh.jpg',
+titulo: 'Clube do Açai',
+valor: 15,
+),
+PacoteIce(
+urlimage:
+'https://play-lh.googleusercontent.com/CaGc3EcRZMsajoobvA-M735
+cbKgTwMpTJDeDbLczarSNmFbDTctjNR5hD8vlwApV7w',
+titulo: 'Açai expresso',
+valor: 10,
+),
+PacoteIce(
+urlimage:
+'https://th.bing.com/th/id/OIP.cqOCqJmMqIRh2ge8Orj0AQHaHa?rs=1
+&pid=ImgDetMain',
+titulo: 'Açai Brasil',
+valor: 15,
+),
+];
+}
