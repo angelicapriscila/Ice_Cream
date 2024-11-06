895b938a1c00526031aff100c64fabd3a5f5bc2b
angelicapriscila <apss11@aluno.ifal.edu.br>
2024-11-06 20:01:59 -0300

    Create databasePicole.dart

---
+++ databasePicole.dart
@@ -0,0 +1,29 @@
+import 'package:helloworld/domain/Card_Picole.dart';
+import 'package:helloworld/domain/Card_Sorvete.dart';
+import 'package:helloworld/domain/card_IceCream.dart';
+class DatabasePicole {
+static List pacoteP = [
+PacotePicole(
+urlimage:
+'https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize/20
+14/12/10/12/WDL-Logo-e-Cartao-de-Visita-48105_92678_065846254_
+970435482.jpg',
+titulo: 'Chupetinha Picolé',
+valor: 5,
+),
+PacotePicole(
+urlimage:
+'https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize/20
+17/05/09/14/Logo-210663_99481_143330262_80178620.jpg',
+titulo: 'Picolé Gourmet',
+valor: 5,
+),
+PacotePicole(
+urlimage:
+'https://s3-sa-east-1.amazonaws.com/projetos-artes/thumb/2014/
+11/19/19/Logo-LV-87524_67451_043701675_1709965975.jpg',
+titulo: 'Gelato Ice',
+valor: 5,
+),
+];
+}
