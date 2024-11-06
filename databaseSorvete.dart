317563dce4e572cc884ab0d1fb9501c3e612d3d4
angelicapriscila <apss11@aluno.ifal.edu.br>
2024-11-06 20:19:23 -0300

    Create databaseSorvete.dart

---
+++ databaseSorvete.dart
@@ -0,0 +1,26 @@
+import 'package:helloworld/domain/Card_Sorvete.dart';
+import 'package:helloworld/domain/card_IceCream.dart';
+class DatabaseSorvete {
+static List pacoteS = [
+PacoteSorvete(
+urlimage:
+'https://th.bing.com/th/id/OIP.em-ENXl0YC5z0LtaJhcQhQAAAA?rs=1
+&pid=ImgDetMain',
+titulo: 'Gelateria Cream',
+valor: 5,
+),
+PacoteSorvete(
+urlimage:
+'https://th.bing.com/th/id/OIP.NeFdiaVisR3KWCAkBIBWNwHaHa?rs=1
+&pid=ImgDetMain',
+titulo: 'Iupy Sorvete',
+valor: 5,
+),
+PacoteSorvete(
+urlimage:
+'https://image.freepik.com/vetores-gratis/logotipo-de-sorveterosa-sorvete_8169-251.jpg',
+titulo: 'Pink Ice',
+valor: 5,
+),
+];
+}
