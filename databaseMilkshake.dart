e8aee820f2c74ec91543b6c510a810dbd64bce29
angelicapriscila <apss11@aluno.ifal.edu.br>
2024-11-06 19:50:57 -0300

    Create databaseMilkshake.dart

---
+++ databaseMilkshake.dart
@@ -0,0 +1,33 @@
+import 'package:helloworld/domain/Card_Sorvete.dart';
+import 'package:helloworld/domain/card_IceCream.dart';
+import 'package:helloworld/domain/card_Milkshake.dart';
+class DatabaseMilkshake {
+static List pacoteM = [
+PacoteMilkshake(
+urlimage:
+'https://thumbs.dreamstime.com/b/%D0%BC%D0%BE%D0%BB%D0%BE%D1%8
+7%D0%BD%D1%8B%D0%B9-%D0%BA%D0%BE%D0%BA%D1%82%D0%B5%D0%B9%D0%BB
+%D1%8C-%D0%B8-%D1%8D%D0%BC%D0%B1%D0%BB%D0%B5%D0%BC%D0%B0-%D0%B
+1%D0%B0%D1%80%D0%BD%D0%BE%D0%B3%D0%BE-%D1%81%D0%B3%D0%BB%D0%B0
+%D0%B6%D0%B8%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F-%D1%81%D0%BB%D0%B0%
+D0%B4%D0%BA%D0%B0%D1%8F-%D0%BD%D0%B0%D0%BF%D0%B8%D1%82%D0%BA%D
+0%BE%D0%B2-157343821.jpg',
+titulo: 'Milk Ice',
+valor: 5,
+),
+PacoteMilkshake(
+urlimage:
+'https://th.bing.com/th/id/R.f1b31cb1165af6d297b8899cef416550?
+rik=LOFpZ4zCDK8FgQ&pid=ImgRaw&r=0',
+titulo: 'Green Cleam',
+valor: 5,
+),
+PacoteMilkshake(
+urlimage:
+'https://img.freepik.com/premium-vector/milkshake-logo-designflat-illustration-fast-food-icon-vector-modern_625890-47.jpg?w
+=826',
+titulo: 'Chocolate Milk',
+valor: 5,
+),
+];
+}
